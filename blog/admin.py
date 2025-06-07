1e2782b5b5fa83d8f38850e4002f268ec0213cea
jonayed-nasir <junayednasir073@gmail.com>
2025-06-07 09:35:07 +0600

    django models v1.1

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Book, Author, FieldType
 
 # Register your models here.
 admin.site.register(Post)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'price')
+    search_fields = ('title', 'author')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email','title')
+
+@admin.register(FieldType)
+class FieldTypeAdmin(admin.ModelAdmin):
+    list_display = ('char_field', 'integer_field', 'email', 'url', 'created_at_dt', 'created_at_date', 'created_at_time', 'updated_at', 'boolean_field', 'text_field', 'float_field', 'decimal_field', 'image', 'file')
+    search_fields = ('char_field', 'email', 'url')
+    list_filter = ('boolean_field',)
+    ordering = ('-created_at_dt',)
--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,41 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
     
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+# common Field types(Model Field)
+# CharField, TextField, IntegerField, FloatField, DecimalField, DateTimeField, DateField, TimeField, BooleanField, EmailField, URLField, FileField, ImageField
+class FieldType(models.Model):
+    char_field = models.CharField(max_length=100)
+    text_field = models.TextField()
+    integer_field = models.IntegerField()
+    decimal_field = models.DecimalField(max_digits=10, decimal_places=2)
+    float_field = models.FloatField()
+    boolean_field = models.BooleanField(default=True)
+    created_at_dt = models.DateTimeField(auto_now_add=True)
+    created_at_date = models.DateField(auto_now_add=True)
+    created_at_time = models.TimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True)
+    url = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='media/',blank=True, null=True)
+    file = models.FileField(upload_to='media/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.char_field} - {self.integer_field} - {self.email} - {self.url} - {self.created_at_dt} - {self.created_at_date} - {self.created_at_time} - {self.updated_at  } - {self.boolean_field} - {self.text_field} - {self.float_field} - {self.decimal_field} - {self.image} - {self.file}'
